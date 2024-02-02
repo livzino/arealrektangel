42d8a9fd9690c4fa0418866212b18833965d5050
Liv <livbendix@live.dk>
2024-02-02 10:06:21 +0100

    uhu

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+udregnRektangelAreal();
+
+function udregnRektangelAreal(width, height) {
+  return width * height;
+}
+
+let beregnetAreal = udregnRektangelAreal(30, 20);
+console.log("beregnetAreal", beregnetAreal);
